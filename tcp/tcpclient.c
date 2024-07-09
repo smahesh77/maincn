39f32fa2eea0b8ee07d7e68a712369beb9b0e152
S Mahesh <98635199+smahesh77@users.noreply.github.com>
2024-07-09 06:48:26 +0530

    Add files via upload

---
+++ tcp/tcpclient.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+int main() { // Corrected the function declaration of main
+    int client;
+    char buffer[1024];
+    struct sockaddr_in servAddr;
+    socklen_t addrSize;
+
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if (client < 0) { // Check if socket creation failed
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(6265);
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Corrected the struct member name
+
+    if (connect(client, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) { // Check if connection failed
+        perror("Connection failed");
+        return 1;
+    }
+    while(1){
+        printf("enter data to send server\n"); // Corrected the newline character
+
+        scanf("%s", &buffer);
+        
+        send(client, buffer, strlen(buffer), 0); // Changed 19 to strlen(buffer)
+
+        
+        if (strcmp(buffer, "exit")==0){
+            printf("stopping connection");
+            break;
+        }
+        recv(client, buffer, 1024, 0);
+        printf("4. Data received: %s\n", buffer); // Added a newline character
+    }
+
+    close(client);
+
+    return 0;
+}
+
